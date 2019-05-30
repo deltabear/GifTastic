d0f1239a73a60841ca4755bfc78b38e2bca5a69a
max delzer <emma.delzer@gmail.com>
2019-05-30 16:10:27 -0700

    Remade function to iterate through array to create buttons. Event listeners grab user input to create new buttons, and button clicks trigger AJAX query to search Giphy API.

--- app.js
+++ app.js
@@ -12,29 +12,67 @@
 // Add form to page that takes user input value and adds it to 'topics' array
     //make a function call that takes each topic in the array remakes the buttons on the page.
 
-function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "";
-      
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    var queryParams = { "api-key": "D8yCtMtas88S6OW60SRHwAih3BqYBUop" };
-      
-
     // Array of buttons
-var topics = ["Optimus Prime", "Megatron", "Transformers G1", "Beast Wars"]
+var bugArray = ["Grasshopper", "Ant", "Wasp", "Honeybee", "Moth", "Butterfly"]
 
 // Function and Loop which will take each part of the array and appends to a button
-function renderButtons() {
-    $("#buttons").empty();
 
-    for (i = 0; i < topics.length; i++) {
-      var a=$("<button>");
-      a.addClass("topic");
-      a.attr("data-name", topics[i]);
-      a.text(topics[i]);
-      $("#buttons").append(a);
+$(document).ready(function() {
+    for (var i = 0; i < bugArray.length; i++) {
+        $("#bug-buttons").append("<button type='button' onclick='searchGif(\"" + bugArray[i] + "\")' class='btn btn-primary' value=' " + bugArray[i] + "'> " + bugArray[i] + " </button>");
+    }
+});
+
+// User click button event, page should grab 10 static gifs from API
+
+function buttonClicked() {
+    var userInput = $('#bug-input').val();
+    searchGif(userInput);
+}
+
+// 
+function submitButtonClicked() {
+    var userInput = $('#bug-input').val();
+
+    if (userInput) {
+        $('#bug-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
     }
 }
 
-// User click button event, page should grab 10 static gifs from API
+function searchGif(gifName) {
+    $.ajax({
+            url: 'https://api.giphy.com/v1/gifs/search?q= ' + gifName + ' &api_key=D8yCtMtas88S6OW60SRHwAih3BqYBUop',
+            type: 'GET',
+        })
+        .done(function(response) {
+            displayGif(response);
+        })
+}
+
+// Gifs will be displayed as still images appended to unique div on html page
+function displayGif(response) {
+    $('#bugs').empty();
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "height:250px"><br>' + rating;
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#bugs').append(image);
+    }
+
+
+//Clicking on the gif will make it dynamic/animated
+    $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+
+    });
+}
